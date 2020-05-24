884f26c7e8b75b77999c019753f2815b9a4695b6
Ershova Anna <lingo9@mail.ru>
2020-05-24 18:17:53 +0300

    запуск автотестов для разных языков интерфейса

--- test_items.py
+++ test_items.py
@@ -5,6 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_cart_button_present(browser):
     browser.get(link)
-    time.sleep(30)
     button = len(browser.find_elements_by_css_selector('.btn-add-to-basket'))
     assert button == 1
